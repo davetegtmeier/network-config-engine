86c62aa007b61f8e23a8bb08756a68c045f5f16d
David Tegtmeier <dtegtmeier@gmail.com>
2026-08-10 13:33:09 -0400

    Load BGP adjacency data

---
+++ src/network_config_engine/bgp_adjacencies.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import csv
+
+def load_bgp_adjacencies(filepath: Path):
+    with open(filepath, mode='r', encoding='utf-8-sig') as file:
+        bgp_adjacencies = []
+        reader = csv.DictReader(file)
+        for adjacency in reader:
+            bgp_adjacencies.append(adjacency)
+
+    return bgp_adjacencies
+
+if __name__ == "__main__":
+    bgp_adjacencies_file = Path("data/bgp_adjacencies.csv")
+    bgp_adjacencies = load_bgp_adjacencies(bgp_adjacencies_file)
+
+    print(bgp_adjacencies)
