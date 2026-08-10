82872dbc9a28b19744477d6a21217d96543a2855
David Tegtmeier <dtegtmeier@gmail.com>
2026-08-10 13:22:37 -0400

    Load external endpoint metadata

---
+++ src/network_config_engine/external_endpoints.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+import yaml
+
+def load_external_endpoints(filepath: Path):
+    with filepath.open("r", encoding="utf-8") as file:
+        data = yaml.safe_load(file)
+
+    return data["external_endpoints"]
+
+if __name__ == "__main__":
+    external_endpoints_file = Path("data/external_endpoints.yaml")
+    external_endpoints = load_external_endpoints(external_endpoints_file)
+
+    print(external_endpoints)
+
